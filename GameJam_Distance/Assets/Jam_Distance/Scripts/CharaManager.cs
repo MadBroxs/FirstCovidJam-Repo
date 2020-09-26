6842e751ba17f486c038decf6e5506b986dee374
MadBroxs <baptiste.valle98@orange.fr>
2020-09-27 00:53:06 +0200

    RandomPerso operationel

--- GameJam_Distance/Assets/Jam_Distance/Scripts/CharaManager.cs
+++ GameJam_Distance/Assets/Jam_Distance/Scripts/CharaManager.cs
@@ -7,12 +7,22 @@ using UnityEngine.UI;
 public class CharaManager : ScriptableObject
 {
     //Variables
-    [Header("Visual")]
-    public Sprite [] hat;
-    public Sprite[] hairs;
-    public Sprite[] masks;
-    public Sprite[] bears;
-    public Sprite[] outfits;
+    [Header("Visual Men")]
+    public Sprite[] body_men;
+    public Sprite[] hat_men;
+    public Sprite[] hairs_men;
+    public Sprite[] masks_men;
+    public Sprite[] bears_men;
+    public Sprite[] outfits_men;
+
+    [Header("Visual Women"), Space(15)]
+    
+    public Sprite[] body_women;
+    public Sprite[] hat_women;
+    public Sprite[] hairs_women;
+    public Sprite[] masks_women;
+    public Sprite[] outfits_women;
+    public Sprite[] accessories_women;
 
     [Header("Color"), Space(25)]
     public Color[] skinColor;
@@ -20,29 +30,95 @@ public class CharaManager : ScriptableObject
 
 
     //Methods
-    public Sprite GetRandomHat()
+    public Sprite GetRandomBody(bool isMale)
     {
-        int num = Random.Range(0, hat.Length);
-        return hat[num];
+        if (isMale)
+        {
+            int num = Random.Range(0, body_men.Length);
+            return body_men[num];
+        }
+        else
+        {
+            int num = Random.Range(0, body_women.Length);
+            return body_women[num];
+        }
     }
-    public Sprite GetRandomHairs()
+    public Sprite GetRandomHat(bool isMale)
     {
-        int num = Random.Range(0, hairs.Length);
-        return hairs[num];
+        if (isMale)
+        {
+            int num = Random.Range(0, hat_men.Length);
+            return hat_men[num];
+        }
+        else
+        {
+            int num = Random.Range(0, hat_women.Length);
+            return hat_women[num];
+        }
     }
-    public Sprite GetRandomMask()
+    public Sprite GetRandomHairs(bool isMale)
     {
-        int num = Random.Range(0, masks.Length);
-        return masks[num];
+        if (isMale)
+        {
+            int num = Random.Range(0, hairs_men.Length);
+            return hairs_men[num];
+        }
+        else
+        {
+            int num = Random.Range(0, hairs_women.Length);
+            return hairs_women[num];
+        }
     }
-    public Sprite GetRandomBear()
+    public Sprite GetRandomMask(bool isMale)
     {
-        int num = Random.Range(0, bears.Length);
-        return bears[num];
+        if (isMale)
+        {
+            int num = Random.Range(0, masks_men.Length);
+            return masks_men[num];
+        }
+        else
+        {
+            int num = Random.Range(0, masks_women.Length);
+            return masks_women[num];
+        }
     }
-    public Sprite GetRandomOutfits()
+    public Sprite GetRandomBear(bool isMale)
     {
-        int num = Random.Range(0, outfits.Length);
-        return outfits[num];
+        if (isMale)
+        {
+            int num = Random.Range(0, bears_men.Length);
+            return bears_men[num];
+
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public Sprite GetRandomOutfits(bool isMale)
+    {
+        if (isMale)
+        {
+            int num = Random.Range(0, outfits_men.Length);
+            return outfits_men[num];
+        }
+        else
+        {
+            int num = Random.Range(0, outfits_women.Length);
+            return outfits_women[num];
+        }
+    }
+
+    public Sprite GetRandomAccessories(bool isMale)
+    {
+        if (isMale)
+        {
+            return null;
+        }
+        else
+        {
+            int num = Random.Range(0, accessories_women.Length);
+            return accessories_women[num];
+        }
     }
 }
--- GameJam_Distance/Assets/Jam_Distance/Scripts/SituationSCO.cs
+++ GameJam_Distance/Assets/Jam_Distance/Scripts/SituationSCO.cs
@@ -8,22 +8,13 @@ public struct Persona
     public string name;
     [Header("Apparence")]
     public bool isMale;
+    public Sprite body;
     public Sprite hat;
     public Sprite hair;
     public Sprite mask;
     public Sprite bear;
     public Sprite outfit;
-
-    public Persona(string name, Sprite hat, Sprite hair, Sprite mask, Sprite bear, Sprite outfit)
-    {
-        this.name = "Philémon";
-        this.isMale = true;
-        this.hat = null;
-        this.hair = null;
-        this.mask = null;
-        this.bear = null;
-        this.outfit = null;
-    }
+    public Sprite accessoires;
 }
 
 
@@ -39,11 +30,19 @@ public class SituationSCO : ScriptableObject
     [Button()]
     public void RandomPerso()
     {
-        character.hat = spriteLib.GetRandomHairs();
-        character.hair = spriteLib.GetRandomHairs();
-        character.mask = spriteLib.GetRandomMask();
-        character.bear = spriteLib.GetRandomBear();
-        character.outfit = spriteLib.GetRandomOutfits();
+        character.body = spriteLib.GetRandomBody(character.isMale);
+        
+        character.hat = spriteLib.GetRandomHat(character.isMale);
+
+        character.hair = spriteLib.GetRandomHairs(character.isMale);
+
+        character.mask = spriteLib.GetRandomMask(character.isMale);
+
+        character.bear = spriteLib.GetRandomBear(character.isMale);
+
+        character.outfit = spriteLib.GetRandomOutfits(character.isMale);
+        
+        character.accessoires = spriteLib.GetRandomAccessories(character.isMale);
     }
 
     [Header("Info"), MultiLineProperty(5)]
